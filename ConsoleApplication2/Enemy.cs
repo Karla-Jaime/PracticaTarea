5f6f3def05cf7644e0f6cc7809524ee6698efc08
Karly-98 <Karly-98@DESKTOP-D2ALSSH>
2018-08-14 17:57:44 -0700

    Agregar archivos de proyecto.

---
+++ ConsoleApplication2/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Enemy
+    {
+        //Autopropertires
+        public int positionX{ get; set; }
+        public int positionY { get; set; }
+        public int explosion { get; set; }
+        public int movement { get; set; }
+        public int points { get; set; }
+        //Propertires
+        private int _shot;
+        public int shot
+        {
+            get
+            {
+                return _shot;
+            }
+            set
+            {
+                if (value >= 4)
+                {
+                    _shot = value;
+                   
+                }
+                else if (value < 4)
+                {
+                    Console.WriteLine("+500POINTS");
+                    Console.WriteLine("Points: " + points + 500);
+                }
+                else if (value == 0)
+                {
+                    Console.WriteLine("You fail");
+                }
+            }
+        }
+
+    }
+}
